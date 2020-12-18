b115f0dc9876c20ec8b2807552b467967bd63423
DevGupta112 <devashish.gupta1998@gmail.com>
2020-12-18 16:07:00 +0530

    New Repo

---
+++ DemoApp/src/com/lti/model/Calc.java
@@ -0,0 +1,10 @@
+package com.lti.model;
+
+public class Calc {
+	public void add(int num1,int num2) {
+		System.out.println("Sum= "+(num1+num2));
+	}
+	public void diff(int num1,int num2) {
+		System.out.println("Difference naya dala= "+(num1-num2));
+	}
+}
---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,15 @@
+package com.lti.model;
+
+public class Sample {
+	public void show() {
+		System.out.println("HelloWorld");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void method() {
+		System.out.println("Hi All, how you doin?");
+	}
+
+}
