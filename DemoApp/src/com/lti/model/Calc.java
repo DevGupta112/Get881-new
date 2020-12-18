e39edc4c4baa24d4300b4adb4a0d0b032389edd4
DevGupta112 <54732775+DevGupta112@users.noreply.github.com>
2020-12-18 16:36:16 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,7 +4,5 @@ public class Calc {
 	public void add(int num1,int num2) {
 		System.out.println("Sum= "+(num1+num2));
 	}
-	public void add2(int num1,int num2) {
-		System.out.println("Sum= "+(num1+num2));
-	}
+	Added bla bla line
 }
