15ea3c0aa02a5624d8a98867346280db6667645c
DevGupta112 <devashish.gupta1998@gmail.com>
2020-12-18 16:41:40 +0530

    added subs method from br1

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,7 +4,7 @@ public class Calc {
 	public void add(int num1,int num2) {
 		System.out.println("Summation= "+"Hello"+(num1+num2));
 	}
-	public void diff(int num1,int num2) {
-		System.out.println("Difference naya dala= "+(num1-num2));
+	public void subs(int num1,int num2) {
+		System.out.println("Difference = "+(num1-num2));
 	}
 }
